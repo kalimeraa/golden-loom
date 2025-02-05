2ecbb51874f7589c621bb1ddee1e0662612eea5e
= <=>
2025-02-05 22:20:24 +0300

    fix: footer

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -32,15 +32,29 @@
             <div class="row my-5 justify-content-center py-5">
                 <div class="col-11">
                     <div class="row ">
-                        <div class="col-xl-8 col-md-4 col-sm-4 col-12   my-auto mx-auto a"><h3 class="text-muted mb-md-0 mb-5 bold-text">Golden Loom</h3></div>
-                        <div class="col-xl-2 col-md-4 col-sm-4 col-12"><h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
-                        <ul class="list-unstyled">
-                            @foreach(\App\Models\Curtain::all() as $curtain)
-                                <li>{{ $curtain->title }}</li>
-                            @endforeach
+                        <div class="col-xl-6 col-md-3 col-sm-3 col-12  my-auto mx-auto a">
+                            <h3 class="text-muted mb-md-0 mb-5 bold-text">Golden Loom</h3>
+                        </div>
+                        <div class="col-xl-2 col-md-3 col-sm-4 col-12"><h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
+                            <ul class="list-unstyled">
+                                @foreach(\App\Models\Curtain::all() as $curtain)
+                                    <li>{{ $curtain->title }}</li>
+                                @endforeach
                             </ul>
-                    </div>
-                        <div class="col-xl-2 col-md-4 col-sm-4 col-12"><h6 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5"><b>ADDRESS</b></h6><p class="mb-1">Istanbul/Turkey</p> </div>
+                        </div>
+                        <div class="col-xl-2 col-md-3 col-sm-3 col-12">
+                            <h6 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5">
+                                <b>ADDRESS</b>
+                            </h6>
+                            <p class="mb-1">Istanbul/Turkey</p> 
+                        </div>
+                        <div class="col-xl-2 col-md-3 col-sm-3 col-12">
+                            <h6 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5">
+                                <b>Diora Berkinova</b>
+                            </h6>
+                            <small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> info@goldenloom.store</small>
+                            <small> <span><i class="fa fa-phone" aria-hidden="true"></i></span> +905320506488 </small>
+                        </div>
                     </div>
                     <div class="row ">
                         <div class="col-xl-8 col-md-4 col-sm-4 col-auto  my-md-0 mt-5 order-sm-1 order-3 align-self-end">
@@ -62,10 +76,6 @@
                                 </a>
                             </p>
                             <small class="rights"><span>&#174;</span> Golden Loom All Rights Reserved.</small>
-                        </div>
-                        <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end "><h6 class="mt-55 mt-2 text-muted bold-text"><b>Diora Berkinova</b></h6>
-                            <small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> info@goldenloom.store</small>
-                            <small> <span><i class="fa fa-phone" aria-hidden="true"></i></span> +905320506488 </small>
                         </div>
                         
                     </div>
